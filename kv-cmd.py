18de65d1b0a2271b023c0740c4b29f4c801a870f
neo-th <jeastcloud@gmail.com>
2025-03-09 16:01:29 -0500

    update

--- kv-cmd.py
+++ kv-cmd.py
@@ -6,7 +6,7 @@ import glob
 import argparse
 import json
 
-def redisPush(store_name, host='localhost', port=6379, password=None, ssl=False):
+def redisPush(store_name, host='localhost', port=6379, password=None, ssl=False, overwrite=False):
 
     r = redis.Redis(host=host, port=port, password=password, ssl=ssl)
 
@@ -14,24 +14,27 @@ def redisPush(store_name, host='localhost', port=6379, password=None, ssl=False)
         print(f"Store file {store_name} does not exist")
         return
     
-    with open(store_name, "r") as file:
-        try:
-            data = json.load(file)
-        except json.JSONDecodeError:
-            print(f"Error: {store_name} is not a valid JSON file.")
-            return
-        
-    all_pushed = True
-    for key, value in data.items():
-        if not r.exists(key):
-            result = r.set(key, value)
-            if not result:
-                all_pushed = False
-    
-    if all_pushed:
-        print(f"{store_name} pushed successfully!")
+    if not is_key_store_file(store_name):
+        print("Invalid key store file!")
     else:
-        print(f"Failed to push {store_name}")
+        with open(store_name, "r") as file:
+            try:
+                data = json.load(file)
+            except json.JSONDecodeError:
+                print(f"Error: {store_name} is not a valid JSON file.")
+                return
+          
+        all_pushed = True
+        for key, value in data["data"].items():
+            if not r.exists(key) or overwrite:
+                result = r.set(key, value)
+                if not result:
+                    all_pushed = False
+        
+        if all_pushed:
+            print(f"{store_name} pushed successfully!")
+        else:
+            print(f"Failed to push {store_name}")
 
 def create_keyValue(key, value, store_name):
 
@@ -43,13 +46,19 @@ def create_keyValue(key, value, store_name):
                 data = {}
     else:
         data = {}
-    
-    data[key] = value
 
-    with open(store_name, "w") as f:
-        json.dump(data, f, indent=4)
+    if "data" not in data:
+        data["data"] = {}
 
-    print(f"Pair add to {store_name}")
+    if is_key_store_file(store_name):
+        data["data"][key] = value
+
+        with open(store_name, "w") as f:
+            json.dump(data, f, indent=4)
+
+        print(f"Pair add to {store_name}")
+    else:
+        print("Invalid key store file!")
 
 def delete_keyValue(key, store_name):
     if not os.path.exists(store_name):
@@ -64,41 +73,77 @@ def delete_keyValue(key, store_name):
         return
 
     if key in data:
-        del data[key]
+        del data["data"][key]
         with open(store_name, "w") as f:
             json.dump(data, f, indent=4)
         print(f"Key deleted from {store_name}!")
     else:
         print(f"Key not found in {store_name}.")
 
-def list_key_stores():
 
+## fix this to only grab store files that have "__tag__"
+def list_key_stores():
+    
     only_json = glob.glob("*.json")
     print(only_json)
 
+## correct if else statement to handle if the file doesnt exists
+def is_key_store_file(store_name):
+
+    try:
+        with open(store_name, "r") as f:
+            data = json.load(f)
+            return data.get("__tag__") == "--key-store--"
+    except json.JSONDecodeError:
+        return False
+
 def view_keyValue(key, store_name):
 
-    found = False
+    if not os.path.exists(store_name):
+        print(f"Store file {store_name} does not exist.")
+        return
 
-    with open(f"{store_name}", "r") as file:
-        for line_number, line in enumerate(file, start=1):
-            if line.startswith(f"{key}"):
-                print(f"{line.split()}")
-                found = True
-                break
-    if not found:
-        print("no <Key:Value> found!")
+    if is_key_store_file(store_name):
+        try:
+            with open(store_name, "r") as f:
+                data = json.load(f)
+        except json.JSONDecodeError:
+            print(f"Error: {store_name} is not a valid JSON file.")
+
+        print(data["data"].get(key, "Key not found!"))
+    else:
+        print("Invalid key store file!")
+
+
 
 def delete_keyStore(store_name):
-    if os.path.exists(store_name):
-        os.remove(store_name)
-        print("Key store deleted!")
+
+    if is_key_store_file(store_name):
+        if os.path.exists(store_name):
+            os.remove(store_name)
+            print("Key store deleted!")
+        else:
+            print("Key store does not exist!")
     else:
-        print("Key store does not exist!")
+        if not os.path.exists(store_name):
+            print("Key store does not exists")
+        else:
+            print("Invalid key store file!")
 
 def new_keyStore(store_name):
-    f = open(f"{store_name}", "x")
-    print("Key store created!")
+    if os.path.exists(store_name):
+        print("File exists!")
+    else:
+        open(store_name, "x")
+
+        data = {}
+        data["__tag__"] = "--key-store--"
+
+        data["data"] = {}
+
+        with open(store_name, "w") as file:
+            json.dump(data, file, indent=4)
+        print("Key store created!")
 
 def main():
 
@@ -127,12 +172,11 @@ def main():
     redis_sync_parser = subparsers.add_parser('redis-sync', help='Sync data from the store file to Redis')
     redis_sync_parser.add_argument('store_name', type=str, help='The store name is a .json file.')
     
-    # optional connection parameters
     redis_sync_parser.add_argument('--host', type=str, default='localhost', help='Redis server hostname (default: localhost)')
     redis_sync_parser.add_argument('--port', type=int, default=6379, help='Redis server port (default: 6379)')
     redis_sync_parser.add_argument('--password', type=str, default=None, help='Password for Redis server (default: None)')
     redis_sync_parser.add_argument('--ssl', action='store_true', help='Enable SSL connection to Redis')
-
+    redis_sync_parser.add_argument('--overwrite', action='store_true', help='If keys are changed and need to be overwritten ')
 
     store_list_parser = subparsers.add_parser('store-list', help='This will list all key stores.')
 
@@ -149,7 +193,7 @@ def main():
     elif args.command == 'create-store':
         new_keyStore(args.store_name)
     elif args.command == 'redis-sync':
-        redisPush(args.store_name, host=args.host, port=args.port, password=args.password, ssl=args.ssl)
+        redisPush(args.store_name, host=args.host, port=args.port, password=args.password, ssl=args.ssl, overwrite=args.overwrite)
     elif args.command == 'store-list':
         list_key_stores()
     else:
